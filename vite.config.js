733f249c7ecef4a23b229070c755c21fb8218917
Zakaria Salmi <zakaria.salmi140302@gmail.com>
2024-09-25 04:34:02 +0200

    img correction

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: "src/assets/*",
-                    dest: "assets",
-                },
-            ],
-        }),
-    ],
+    plugins: [vue()],
 });
